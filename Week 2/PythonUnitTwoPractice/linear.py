2b209eb110cf510eb9583f52be405cab367b1510
Kelvin Cao <kelvincaoyx@gmail.com>
2021-07-13 16:38:34 -0400

    Added comments

--- Week 2/PythonUnitTwoPractice/linear.py
+++ Week 2/PythonUnitTwoPractice/linear.py
@@ -2,6 +2,7 @@
 A calculator for a linear equation
 '''
 
+#Checks if the equation can be solved/ returns the answer
 def linearEquationSolver(a,b,c):
     if a == "" or b == "" or c == "" or a == "0":
         return None
@@ -10,15 +11,17 @@ def linearEquationSolver(a,b,c):
     
     return x
 
-def helper(a,b,c,solution):
-    if solution == None:
-        return "The values of your parameters are invalid"
-    return "The solution of your equation " + str(a) + "x + " + str(b) + " = " + str(c) + " is x = " + str(solution)
-
+#Header of the calculator
 print("This calculator solves an equation of the form ax+b=c.")
 
 a = input("Enter the value of the parameter a: ")
 b = input("Enter the value of the parameter b: ")
 c = input("Enter the value of the parameter c: ")
 
-print(helper(a,b,c,linearEquationSolver(a,b,c)))
+#checks if the equation is solvable and can output an error message
+if linearEquationSolver(a,b,c) == None:
+    print("The values of your parameters are invalid")
+
+#outputs the answer of the equation
+else:
+    print("The solution of your equation " + a + "x + " + b + " = " + c + " is x = " + str(linearEquationSolver(a,b,c)))
--- Week 2/PythonUnitTwoPractice/shop.py
+++ Week 2/PythonUnitTwoPractice/shop.py
@@ -1,25 +1,30 @@
 '''
-Helps the user calculate the tax that they should charge the customer
+Helps the user calculate the price that they should charge the customer
 '''
 
 def tax():
+    #Checks if the user is from ontario or not
     location = input("Are you shopping from Ontario? Enter Y or N: ")
     
+    #Makes the user input a tax rate if they are not from ontario
     if location == "n" or location == "N":
         taxRate = input("Enter the tax of your state or province as a percentage: ")
 
+    #Gives the user the default rate of 13 percent if they are from ontario
     if location == "y" or location == "Y":
         taxRate = "13"
 
+    #Gets some information from the user to help us calculate the final price
     price = float(input("Enter the price of the item: "))
     age = int(input("Enter your age: "))
     member = input("Are you a member of this store? Enter Y or N: ")
 
+    #Gives the customer a discount if they fulfill all the proper conditions
     if (age < 18 or age >= 65) and (member == "y" or member == "Y"):
         price = price * 0.85
 
     return str(round((price * float("1." + taxRate)),2))
 
-
+#prints out the final price that the user will need to pay
 print("The total cost with tax for your order is " + tax())
         
