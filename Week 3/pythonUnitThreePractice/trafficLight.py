504d5d15160ca3dd46a9d0366bb36e83da54d72a
Kelvin Cao <kelvincaoyx@gmail.com>
2021-07-20 20:13:47 -0400

    comments

--- Week 3/pythonUnitThreePractice/trafficLight.py
+++ Week 3/pythonUnitThreePractice/trafficLight.py
@@ -1,23 +1,27 @@
 '''This function is suppoed to emulate a traffic light system'''
 
-#this function helps the program decide what to do after receving an input from the user
-def currentLight(choice):
-    if choice == "b" or choice == "B":
-        print("Green")
-    elif choice == "y" or choice == "Y":
-        print("Red")
-    elif choice == "r" or choice == "R":
-        print("Blue")
-    elif choice == "g" or choice == "G":
-        print("Yellow")
-    elif choice == "q" or choice == "Q":
-        print("Good bye!")
+#this function helps the program decide what to do after receving a colour from the user
+def currentLight(colour):
+    if colour == "b" or colour == "B":
+        print("\nGreen\n")
+    elif colour == "y" or colour == "Y":
+        print("\nRed\n")
+    elif colour == "r" or colour == "R":
+        print("\nBlue\n")
+    elif colour == "g" or colour == "G":
+        print("\nYellow\n")
     else:
-        print("Invalid Colour")
+        print("\nInvalid Colour\n")
 
-#allows the user to make a choice of colours or exit
-choice = input("Enter a colour choice (q to quit)\
-    \n(B or b, G or g, Y or y, R or r) ")
+def inputHelper():
+    #allows the user to make a choice of colours or exit
+    choice = input("Enter a colour choice (q to quit)\
+        \n(B or b, G or g, Y or y, R or r) ")
+    while choice != "q" and choice != "Q":
+        currentLight(choice)
+        choice = input("Enter a colour choice (q to quit)\
+        \n(B or b, G or g, Y or y, R or r) ")
+    print("\nGood bye!\n")
 
 #runs the function
-currentLight(choice)
+inputHelper()
